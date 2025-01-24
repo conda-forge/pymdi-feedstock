b17ddab1a524be18fc79f12a86c0b790603d4946
Taylor Barnes <taylor.a.barnes@gmail.com>
2025-01-24 00:18:49 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,8 +9,11 @@ cmake -Bbuild \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_INSTALL_LIBDIR=lib \
-    -Dpython_package=ON \
-    -Dlanguage=Python \
+    -DBUILD_SHARED_LIBS=ON \
+    -DMDI_Fortran=OFF \
+    -DMDI_Python=ON \
+    -DMDI_CXX=OFF \
+    -DMDI_Python_PACKAGE=ON
     -Dpython_version=$(python -c "import sys; print(str(sys.version_info[0])+'.'+str(sys.version_info[1])+'.'+str(sys.version_info[2]))")
 
 # Build step
