048bc9fb9c0c19d6ef1dc2c85e68fc394adf1e8c
Taylor Barnes <taylor.a.barnes@gmail.com>
2026-04-09 09:08:40 -0500

    Add MPI_CMAKE_ARGS to build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,15 @@ else
   MDI_MPI=ON
 fi
 
+# When cross-compiling, CMake cannot execute the arm64 mpicc wrapper to
+# auto-detect MPI.  Provide the paths directly using CMake 3.17+ FindMPI hints.
+MPI_CMAKE_ARGS=""
+if [[ "${MDI_MPI}" == "ON" ]] && [[ "${build_platform}" != "${target_platform}" ]]; then
+  MPI_CMAKE_ARGS="-DMPI_C_HEADER_DIR=${PREFIX}/include \
+    -DMPI_C_LIB_NAMES=mpi \
+    -DMPI_mpi_LIBRARY=${PREFIX}/lib/libmpi${SHLIB_EXT}"
+fi
+
 # Configure step
 cmake ${CMAKE_ARGS} -GNinja -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
@@ -18,6 +27,7 @@ cmake ${CMAKE_ARGS} -GNinja -DCMAKE_BUILD_TYPE=Release \
     -DMDI_Python=ON \
     -DMDI_Python_VERSION=$(python -c "import sys; print(str(sys.version_info[0])+'.'+str(sys.version_info[1])+'.'+str(sys.version_info[2]))") \
     -DMDI_USE_MPI=${MDI_MPI} \
+    ${MPI_CMAKE_ARGS} \
     ..
 # Build step
 ninja
